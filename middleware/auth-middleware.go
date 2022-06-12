48f3d1eba1f05dc551dd8e54ab00b20bf64e52b0
stkr89 <sumit.tokkar@gmail.com>
2022-06-12 09:15:24 -0500

    validate user in auth middleware

--- middleware/auth-middleware.go
+++ middleware/auth-middleware.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/go-kit/kit/endpoint"
+	"github.com/go-playground/validator/v10"
 	"github.com/stkr89/modelsvc/common"
 	"github.com/stkr89/modelsvc/types"
 )
@@ -11,16 +12,22 @@ import (
 func AuthenticateUser() endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, request interface{}) (interface{}, error) {
-			userBytes := ctx.Value("user")
+			errMsg := "unauthorized access"
 
+			userBytes := ctx.Value("user")
 			if userBytes == nil {
-				return nil, common.NewError(common.Unauthorized, "unauthorized access")
+				return nil, common.NewError(common.Unauthorized, errMsg)
 			}
 
 			var user types.User
 			err := json.Unmarshal(userBytes.([]byte), &user)
 			if err != nil {
-				return nil, common.NewError(common.Unauthorized, "unauthorized access")
+				return nil, common.NewError(common.Unauthorized, errMsg)
+			}
+
+			err = validator.New().Struct(user)
+			if err != nil {
+				return nil, common.NewError(common.Unauthorized, errMsg)
 			}
 
 			return next(ctx, request)
