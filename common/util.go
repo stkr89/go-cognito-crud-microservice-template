52ff225cb127932d982e3e43a2c4785222cd7e28
stkr89 <sumit.tokkar@gmail.com>
2022-06-12 11:18:19 -0500

    updated error massage

--- common/util.go
+++ common/util.go
@@ -7,15 +7,17 @@ import (
 )
 
 func GetUserFromContext(ctx context.Context) (*types.User, error) {
+	errMsg := "user not found"
+
 	userBytes := ctx.Value("user")
 	if userBytes == nil {
-		return nil, NewError(UserNotFoundInContext, UserNotFoundInContext)
+		return nil, NewError(UserNotFoundInContext, errMsg)
 	}
 
 	var user types.User
 	err := json.Unmarshal(userBytes.([]byte), &user)
 	if err != nil {
-		return nil, NewError(UserNotFoundInContext, UserNotFoundInContext)
+		return nil, NewError(UserNotFoundInContext, errMsg)
 	}
 	return &user, nil
 }
